8bf3600c33e7e907a63d01ca4a343b67158eee6b
stephannygoncalves <sagdr@aluno.ifnmg.edu.br>
2022-06-03 22:14:24 -0300

    1 file modified

--- pesoideal.js
+++ pesoideal.js
@@ -3,12 +3,12 @@ function calcularIMC() {
   var altura = document.getElementById("altura").value; 
  
   if (sexo == "F" || sexo == "f") { 
-    var pesoIdeal = (62.1 * altura) - 44.7; 
+    var pesoIdeal = (67.4 * altura) - 38.3; 
     var pesoArredondado = parseFloat(pesoIdeal.toFixed(2)) 
     alert("O seu peso ideal é: " + pesoArredondado + "Kg") 
   } 
   else if (sexo == "M" || sexo == "m") { 
-    var pesoIdeal = (72.7 * altura) - 58; 
+    var pesoIdeal = (80.9 * altura) - 60; 
     var pesoArredondado = parseFloat(pesoIdeal.toFixed(2)) 
     alert("O seu peso ideal é: " + pesoArredondado + "Kg") 
   } 
