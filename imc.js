ce9a0efa76f824e667bee6fe07296ce931606fa1
stephannygoncalves <sagdr@aluno.ifnmg.edu.br>
2022-06-03 22:27:53 -0300

    1 file modified

--- imc.js
+++ imc.js
@@ -1,9 +1,7 @@
 function calcular(){
   
-
   var formulario = document.getElementById("formulario");	
-  	
-    
+  	 
   var kilos  		= +formulario.kilos.value;
   var metros 		= +formulario.metros.value;
   var centimetros = +formulario.centimetros.value;
